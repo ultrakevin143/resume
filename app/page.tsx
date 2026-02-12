bf84fb027c4baa8f92067a335b0e53579180e972
ultrakevin143 <118041134+ultrakevin143@users.noreply.github.com>
2026-02-12 23:19:32 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -117,7 +117,7 @@ const RESUME_DATA: ResumeData = {
       description: "Ecommerce computer parts products",
       tech: ["React.js"],
       imageUrl: "shop.jpg", // Replace with project image
-      projectUrl: "https://lazappyv3.vercel.app/" // Replace with live link
+      projectUrl: "https://mysteriousq-sms-bomber.onrender.com/" // Replace with live link
     }
   ],
   certificates: [
